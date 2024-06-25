611171535b6b00660a73e75903f37172a1672301
Beaprofessor <professornberlin2122@gmail.com>
2024-06-25 23:03:34 +0530

    Added codes for downloading and populating the category List at the homePage

---
+++ js/index.js
@@ -0,0 +1,29 @@
+console.log("Index file of JS")
+
+async function fetchCategories(){
+    // this function is marked async so this will also return a promise
+       const response =   await fetch("https://fakestoreapi.com/products/categories")
+       const data = await response.json()
+       return data
+}
+
+async function populateCategories(){
+    const categories = await fetchCategories()
+    const categoryList = document.getElementById('categoryList')
+    categories.forEach(category => {
+        const categoryHolder = document.createElement('div')
+        const categoryLink = document.createElement('a')
+
+
+        categoryLink.textContent = category           // setting the category name as the text of the anchor tag
+        categoryLink.href = '#'
+        categoryHolder.classList.add('category-item', 'd-flex' ,  'align-items-center' , 'justify-content-center')
+        categoryHolder.appendChild(categoryLink)
+
+        categoryList.appendChild(categoryHolder)
+    })
+
+
+}
+
+populateCategories()
